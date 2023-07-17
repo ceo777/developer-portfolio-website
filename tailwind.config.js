12a02f786ee26745bd7b3744b215b33140f9fe5b
Oleg Dubnov <61102565+ceo777@users.noreply.github.com>
2023-07-17 13:22:41 +0300

    The first version of the website. Navbar and Hero section were added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '2rem',
+        sm: '3rem',
+        md: '2rem',
+        lg: '3rem',
+        xl: '8rem',
+        '2xl': '8rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
